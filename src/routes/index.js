338894ccd3135fb3520b0f9a3cd8d22e89d98a42
emanuele-rosa <emanuele.rosa@startse.com>
2024-09-04 16:46:38 -0300

    refactor: replace login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
+
+const { userLogin } = require("../controllers/userController");
 
 router.get("/", (req, res) => {
-    res.json({status: true, msg: "Welcome to your events management!"})
-})
+  res.json({ status: true, msg: "Welcome to your events management!" });
+});
+router.post("/login", userLogin);
 
 module.exports = router;
