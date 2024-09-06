98bc1b09a49a3df115b94d99a1bff70e92aad2a2
emanuele-rosa <emanuele.rosa@startse.com>
2024-09-06 19:47:44 -0300

    fix: remove unnecessary validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 const { userLogin } = require("../controllers/userController");
 const { createAdminUser } = require("../controllers/indexController");
-const { validateToken } = require("../helpers/auth");
 
-router.post("/", validateToken, createAdminUser);
+router.post("/", createAdminUser);
 router.post("/login", userLogin);
 
 module.exports = router;
